d05dc3bea45ad42858a4beed367aef4668503dde
Evan Fierst <efierst@weld.com>
2017-04-12 12:37:15 -0400

    (WIP) Added missing files from src directory, including class defs for cfb_types and altium_lib

---
+++ src/altium_lib.cpp
@@ -0,0 +1,151 @@
+#include "altium_lib.hpp"
+
+altium_lib::altium_lib(void)
+{
+
+}
+
+altium_lib::altium_lib(const std::string & path_to_file)
+{
+    this->filepath_ = path_to_file;
+}
+
+bool altium_lib::load_from_file(const std::string & path_to_file)
+{
+    std::string file_to_load;
+
+    if(path_to_file.empty())
+    {
+        if(filepath_.empty())
+        {
+            return false;
+        }
+        else
+        {
+            file_to_load = filepath_;
+        }
+    }
+    else
+    {
+        file_to_load = path_to_file;
+    }
+
+    // Create the file stream object
+    fs::ifstream ifs;
+
+    // Path to the library, specified by user at the command line
+    fs::path libfile(file_to_load);
+
+    // Check to make sure it's actually a valid file
+    if(exists(libfile))
+    {
+        if(is_directory(libfile))
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+
+    ifs.open(libfile, std::ios::binary | std::ios::ate);
+
+    // Seek to the beginning of the file
+    ifs.seekg(0, std::ios::beg);
+
+    // Build the header
+    build_header(ifs);
+
+    // build the sector allocation table
+    build_sat(ifs);
+
+    build_dir_entry_chain(ifs);
+
+    ifs.close();
+
+    return true;
+}
+
+bool altium_lib::get_library_entries(std::vector<std::string> & list_of_entries)
+{
+    // Clear the entries vector and set its size to zero
+    list_of_entries.erase(list_of_entries.begin(), list_of_entries.end());
+
+    std::string current_name;
+
+    for(size_t i = 0; i < dir_entry_tree.size(); i++)
+    {
+        current_name = dir_entry_tree.at(i).get_name();
+
+        if( dir_entry_tree.at(i).get_object_type() == dir_entry_object_type::STORAGE &&
+            altium_lib_keywords.find(current_name) == altium_lib_keywords.end())
+        {
+            list_of_entries.push_back(current_name);
+        }
+    }
+
+    // Return true if there are any entries to read
+    return (!list_of_entries.empty());
+}
+
+void altium_lib::build_header(std::ifstream &cfb_file)
+{
+    cfb_file.read(reinterpret_cast<char *>(&header_), sizeof(header_t));
+}
+
+void altium_lib::build_sat(std::ifstream &cfb_file)
+{
+    uint32_t offset;
+
+    uint32_t sector_size = (1 << header_.sec_size);
+
+    int32_t sector_id;
+
+    for(uint32_t i = 0; i < header_.sec_count; i++)
+    {
+        offset = (1 + header_.master_sec_ids[i]) * (sector_size);
+        cfb_file.seekg(offset, std::ios::beg);
+        for(uint32_t j = 0; j < sector_size / sizeof(uint32_t); j++)
+        {
+            cfb_file.read(reinterpret_cast<char *>(&sector_id), sizeof(int32_t));
+            sector_alloc_table.push_back(sector_id);
+        }
+    }
+}
+
+void altium_lib::build_dir_entry_chain(std::ifstream &cfb_file)
+{
+    uint32_t offset = 0;
+
+    int32_t current_sector = header_.first_sec_id;
+
+    uint32_t sector_size = (1 << header_.sec_size);
+
+    uint32_t dir_entries_per_sector = sector_size / (CFB_DIR_ENTRY_SIZE);
+
+    while(current_sector != ENDOFCHAIN)
+    {
+        offset = (1 + current_sector) * (sector_size);
+
+        cfb_file.seekg(offset, std::ios::beg);
+
+        for(uint8_t i = 0; i < dir_entries_per_sector; i++)
+        {
+            read_next_directory_entry(cfb_file);
+        }
+        // Move to the next sector containing directory entries
+        current_sector = sector_alloc_table.at(current_sector);
+    }
+}
+
+void altium_lib::read_next_directory_entry(std::ifstream &cfb_file)
+{
+    uint8_t dir_entry_bytes[CFB_DIR_ENTRY_SIZE];
+
+    cfb_file.read(reinterpret_cast<char *>(&dir_entry_bytes), CFB_DIR_ENTRY_SIZE);
+
+    directory_entry new_dir_entry(dir_entry_bytes, sizeof(dir_entry_bytes));
+
+    dir_entry_tree.push_back(new_dir_entry);
+}
---
+++ src/cfb_types.cpp
@@ -0,0 +1,92 @@
+#include "cfb_types.hpp"
+
+directory_entry::directory_entry()
+{
+	this->dir_entry_bytes.resize(CFB_DIR_ENTRY_SIZE);
+}
+
+directory_entry::directory_entry(const directory_entry & to_copy)
+{
+	this->dir_entry_bytes.resize(CFB_DIR_ENTRY_SIZE);
+	this->dir_entry_bytes = to_copy.dir_entry_bytes;
+}
+
+directory_entry::directory_entry(uint8_t * dir_entry_bytes, size_t bytes_to_read)
+{
+	this->dir_entry_bytes.resize(CFB_DIR_ENTRY_SIZE);
+	this->assign_bytes(dir_entry_bytes, bytes_to_read);
+}
+
+void directory_entry::assign_bytes(uint8_t * dir_entry_bytes, size_t bytes_to_read)
+{
+	this->dir_entry_bytes = std::valarray<uint8_t>(dir_entry_bytes, bytes_to_read);
+}
+
+std::string directory_entry::get_name()
+{
+	std::string name_string = "";
+	
+	// Find "name" entry in map
+	std::valarray<uint8_t> name_bytes = get_dir_entry_param_bytes("name");	
+	
+	// If we have at least one char16_t to read
+	if(name_bytes.size() > sizeof(char16_t))
+	{
+		// Convert it to a string because UTF-16 strings are very bad + unprintable
+		std::u16string u16name(reinterpret_cast<char16_t *>(&name_bytes[0]), name_bytes.size() / sizeof(char16_t));
+		std::wstring_convert<std::codecvt_utf8<char16_t>, char16_t> conv;
+		name_string = std::string(conv.to_bytes(u16name));
+	}
+
+	// This string will be empty if there was no name found
+	return name_string;
+}
+
+std::string directory_entry::get_creation_time()
+{
+	return "";
+}
+
+std::string directory_entry::get_modified_time()
+{
+	return "";
+}
+
+dir_entry_object_type directory_entry::get_object_type()
+{
+	dir_entry_object_type ot = (dir_entry_object_type)(get_dir_entry_param_bytes("type")[0]);
+
+	return ot;
+}
+
+// TODO: Template function that returns the value/type we're actually interested in?
+std::valarray<uint8_t> directory_entry::get_dir_entry_param_bytes(std::string param_key)
+{
+	// Find entry in map
+	std::map<std::string, std::pair<uint8_t, uint8_t>>::iterator it = dir_entry_params.find(param_key);
+
+	// If the item actually exists
+	if(it != dir_entry_params.end())
+	{
+		// Get the offset and the size (in bytes)
+		uint8_t offset = it->second.first;
+		uint8_t size = it->second.second;
+		// Grab a slice based on 
+		std::valarray<uint8_t> bytes(dir_entry_bytes[std::slice(offset,size, 1)]);
+		return bytes;
+	}
+
+	// If no param was found, or there was an error, return an empty valarray
+	return std::valarray<uint8_t>();
+}
+
+// TODO: Not implemented yet
+uint32_t directory_entry::get_stream_sector_id()
+{
+	return FREESECT;
+}
+
+uint32_t directory_entry::get_stream_size()
+{
+	return FREESECT;
+}
